3a682f0e9730660603d2fdec673c0964ad70197d
Aria Assadi <aria.assadi@gmail.com>
2025-04-16 15:54:22 +0200

    Made scroll even better

--- components/ChatInterface/MessageContainer.tsx
+++ components/ChatInterface/MessageContainer.tsx
@@ -96,7 +96,7 @@ MessageItem.displayName = "MessageItem";
 const MessageContainer: React.FC<MessageContainerProps> = React.memo(
   ({ messages, error, toolCall, isLoading, showCitation, messagesEndRef }) => {
     return (
-      <div className="flex-1 overflow-y-auto space-y-4 w-full pt-2">
+      <div className="flex-1 overflow-y-auto space-y-4 w-full pt-4">
         <AnimatePresence initial={false}>
           {messages.map(
             (message: Message) =>
